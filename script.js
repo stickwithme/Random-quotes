58a877eff08660e7dc5836afc22a97718db8da74
Alexnader <stickwithme@mail.ru>
2024-11-07 00:26:40 +0300

    Модифицированы файлы index.html, script.js и style.css

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 const quotes = [
-  'Успех — это способность идти от поражения к поражению, не теряя энтузиазма. - Уинстон Черчиль',
-  'Будьте той переменой, которую хотите увидеть в мире. - Махатма Ганди',
-  'Выберите себе работу по душе, и вам не придется работать ни одного дня в своей жизни. - Конфуций',
+  {
+    quote:
+      'Успех — это способность идти от поражения к поражению, не теряя энтузиазма.',
+    author: 'Уинстон Черчиль',
+  },
+  {
+    quote: 'Будьте той переменой, которую хотите увидеть в мире',
+    author: 'Махатма Ганди',
+  },
+  {
+    quote:
+      'Выберите себе работу по душе, и вам не придется работать ни одного дня в своей жизни',
+    author: 'Конфуций',
+  },
+  {
+    quote:
+      'Единственный способ сделать что-то очень хорошо — любить то, что ты делаешь.',
+    author: 'Стив Джобс',
+  },
+  {
+    quote: 'Секрет успеха — это делать обычные вещи необыкновенно хорошо.',
+    author: 'Джон Д. Рокфеллер',
+  },
+  {
+    quote: 'Трудности — это шанс стать лучше.',
+    author: 'Дюк Эллингтон',
+  },
+  {
+    quote: 'Неудача — это просто возможность начать снова, но уже более мудро.',
+    author: 'Генри Форд',
+  },
+  {
+    quote:
+      'Каждая великая мечта начинается с мечтателя. Всегда помните, у вас есть сила, терпение и страсть, чтобы достичь звезд и изменить мир.',
+    author: 'Гарриет Тубман',
+  },
+  {
+    quote: 'Мудрость — это способность держать дистанцию от своих эмоций.',
+    author: 'Далай-лама',
+  },
+  {
+    quote:
+      'Чтобы достичь величия, мы должны не только действовать, но и мечтать, не только планировать, но и верить.',
+    author: 'Анатоль Франс',
+  },
 ]
 
 const quoteElement = document.getElementById('quote')
@@ -10,9 +52,10 @@ const generateBtn = document.getElementById('generate-btn')
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length)
   const randomQuote = quotes[randomIndex]
-  quoteElement.textContent = randomQuote
+  const quote = `<em>" ${randomQuote.quote}"</em><br> <span id="author">${randomQuote.author}</span>`
+  quoteElement.innerHTML = quote
 }
 
 generateBtn.addEventListener('click', generateRandomQuote)
 
-generateRandomQuote()
+// generateRandomQuote()
